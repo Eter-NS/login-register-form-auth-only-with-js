771bb2f6b0dbbf241ae6050aad188a50a82deaf4
EterNorSos <norbert.sosnowski@outlook.com>
2023-01-31 00:56:49 +0100

    Adding opacityListener and form validation script

--- src/js/register.js
+++ src/js/register.js
@@ -1,2 +1,13 @@
 import "../scss/main.scss";
 import "../scss/register components/register.scss";
+import opacityListener from "./components/label_opacity_listener";
+import hashFunc from "./components/hash_func";
+import RegisterAuth from "./components/register_auth";
+// hashFunc(text).then((digestHex) => console.log(digestHex));
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  document.querySelectorAll(".form--input-container").forEach((el) => {
+    opacityListener(el.lastElementChild, el.firstElementChild);
+  });
+  const form = new RegisterAuth(".form");
+});
