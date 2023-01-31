8d1b07a9681754b7e3e5be2a23c5aaca9b665600
EterNorSos <norbert.sosnowski@outlook.com>
2023-01-31 01:00:29 +0100

    email duplicate script module (register&login usage)

---
+++ src/js/components/doesTheEmailExists.js
@@ -0,0 +1,14 @@
+export default function doesTheEmailExist(dataObject, emailInput) {
+  let emailHash,
+    exists = false;
+  hashMessage(emailInput.value).then((hashedValue) => {
+    emailHash = hashedValue;
+  });
+  for (const key in Object.keys(dataObject)) {
+    if (key === emailHash) {
+      exists = true;
+    }
+  }
+
+  return exists;
+}
