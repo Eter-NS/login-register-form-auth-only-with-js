626244d5bf48763ef7c46f04e531c6e64f74bcff
EterNorSos <norbert.sosnowski@outlook.com>
2023-02-03 00:40:47 +0100

    Adding placeholder styling for login

--- src/js/login.js
+++ src/js/login.js
@@ -1,2 +1,11 @@
 import "../scss/main.scss";
 import "../scss/login components/login.scss";
+import opacityListener from "./components/label_opacity_listener";
+import loginAuth from "./components/login_auth";
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  document.querySelectorAll(".form--input-container").forEach((el) => {
+    opacityListener(el.lastElementChild, el.firstElementChild);
+  });
+  const loginForm = new loginAuth(".form");
+});
